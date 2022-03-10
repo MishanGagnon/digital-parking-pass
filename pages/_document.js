6a5ac8b9c3b23a27ba4bf07f21003b65b4288c70
Mishan <mishangagnon@gmail.com>
2022-03-09 21:44:04 -0800

    fixed pwa icons on IOS

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,11 @@ class MyDocument extends Document {
           <link rel="manifest" href="/manifest.json" />
           <link rel="apple-touch-icon" href="/icon.png"></link>
           <meta name="theme-color" content="#fff" />
+          <meta charset="utf-8" />
+          <link href="apple-touch-icon.png" rel="apple-touch-icon"/>
+    <link href="apple-touch-icon.png" rel="apple-touch-icon" sizes="76x76"/>
+<link href="apple-touch-icon.png" rel="apple-touch-icon" sizes="120x120"/>
+<link href="apple-touch-icon.png" rel="apple-touch-icon" sizes="152x152"/>
         </Head>
         <body>
           <Main />
